e2eb87335196f47992679b294c03e0c75354c24d
dharshan_bj <bjdharshan@gmail.com>
2018-04-08 18:16:03 -0700

    Implemented a new system call for the xv6 OS to dump memory of a process on the console and annotated text, guard, stack, and heap memory sections of a process.

---
+++ dump.c
@@ -0,0 +1,46 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "syscall.h"
+
+#define PGSIZE 4096
+
+int main(int argc, char *argv[])
+{
+
+  /* Fork a new process to play with */
+  /* We don't have a good way to list all pids in the system
+     so forking a new process works for testing */ 
+  int pid = fork();
+
+  if (pid == 0) {
+    /* child spins and yields */
+    while (1) {
+       sleep(5);
+    };
+  }
+ /* parent dumps memory of the child */
+	int size=(int)sbrk(0);
+	char *buffer=malloc(size);
+	char *addr=0;
+	dump(pid,addr,buffer,size);
+	int *ptr=(int*)buffer;
+	int i=0;
+	for(i=0;i<size;i+=16)
+	{
+		printf(1,"0x%x\t",i);
+		printf(1,"0x%x\t",*ptr);
+		ptr++;
+		printf(1,"0x%x\t",*ptr);
+		ptr++;
+		 printf(1,"0x%x\t",*ptr);
+		ptr++;
+ 		 printf(1,"0x%x\t",*ptr);
+		ptr++;
+	       	printf(1,"\n");
+		
+	}
+
+  exit();
+}
+
